96f49a5499e611ff4ca652b479d24eb3363f02dd
DingodiIe <52380641+DingodiIe@users.noreply.github.com>
2019-07-19 00:02:49 -0400

    Create animater.cs

---
+++ Maze/Assets/animater.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animater : MonoBehaviour
+{
+    // Start is called before the first frame update
+      Animator anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator> ();
+       // dir = GetComponent<Sprite Renderer> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey (KeyCode.RightArrow)){
+            anim.SetInteger("New Int", 1);
+        }
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) <= 0.1){
+             
+        anim.SetInteger("New Int", 0);
+       }
+       if (Mathf.Abs(Input.GetAxis("Vertical")) <= .001){
+           anim.SetInteger("New Int", 2);}
+        }}
